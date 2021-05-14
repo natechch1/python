392fe9c06e323a12e60168da3c7149c046d8e16e
natechch1 <82234988+natechch1@users.noreply.github.com>
2021-05-14 14:31:27 -0400

    Update client.py

--- python/chat_room/local/client.py
+++ python/chat_room/local/client.py
@@ -1,72 +1,94 @@
 import socket
 import threading
+import sys
+import os
+
 
 HEADER = 16
 PORT = 5050
-FORMAT = 'utf-8'
-SERVER = "192.168.2.27"
+SERVER = socket.gethostbyname(socket.gethostname()) # get the IPv4 address
+if SERVER.startswith('127.0.'):
+    SERVER = '0.0.0.0'
+
 ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
 DISCONNECT = "!DISCONNECT"
+# create to store all client and used for sending the msg from other client 
+list_of_client = {}
+
+#create a new socket, AF_INET is type? and SOCK_STREAM is method?
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+server.bind(ADDR) 
 
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
-# if len(sys.argv) != 3: 
-#     print ("Correct usage: script, IP address, port number")
-#     exit() 
-# IP_address = str(sys.argv[1]) 
-# Port = int(sys.argv[2]) 
-# client.connect((IP_address, Port)) 
 
-def send():
-    try:
-        while True:
-            clientChat = input()
-            if clientChat == "stop":
-                client.send(DISCONNECT.encode())
-                break
-            # message = clientChat.encode(FORMAT)
-            # message_length = len(message)
-            # send_length = str(message_length).encode(FORMAT)
-            # send_length += b' ' * (HEADER - len(send_length))
-            # client.send(send_length)
-            client.send(clientChat.encode())
-    except Exception:
-        print(Exception)
-
-        
-       
+def handle_client(connect, address):
+    print(f"[NEW CONNECTION] {address} connected.")
     
+    connected = True
+    while connected:
+        # msg_length = connect.recv(HEADER).decode(FORMAT) # get the msg length from client but type is string
+        # if msg_length: # avoid the msg is null
+        #     msg_length = int(msg_length)  # translate it to int
+        #     msg = connect.recv(msg_length).decode(FORMAT) # receiving the acual msg 
+            
+        #     msg_to_send = "<" + str(address[1]) + "> " + msg
+        message = connect.recv(1024).decode()
+        if message:
+            if message == DISCONNECT:
+                connected = False
+            if len(list_of_client) > 1:
+                broadcast(message, connect)
+            print(f"[{address}] {message}")
+    connect.close()      
+            
+        
+def broadcast(msg, conn):
+    try:
+        for client in list_of_client:
+            if client != conn:
+                    # message = msg.encode(FORMAT)
+                    # message_length = len(message)
+                    # send_length = str(message_length).encode(FORMAT)
+                    # send_length += b' ' * (HEADER - len(send_length))
+                    msg = "[" + str(list_of_client.get(conn)) + "] " + msg + "\n"
+                    client.send(msg.encode())
+        if msg == DISCONNECT:
+            list_of_client.pop(conn)    
 
-def receive():
-    #  for message in iter(lambda: client.recv(1024).decode(), ''):
-    #     print(":", message)
-    #     print("")
+    except BrokenPipeError: 
+        # Python flushes standard streams on exit; redirect remaining output 
+        # to devnull to avoid another BrokenPipeError at shutdown 
+        devnull = os.open(os.devnull, os.O_WRONLY) 
+        os.dup2(devnull, sys.stdout.fileno()) 
+        sys.exit(1)  # Python exits with error code 1 on EPIPE 
+                        
+
+
+def start():
+    server.listen(100)
+    print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
-        print(client.recv(1024).decode())
+        connect, address = server.accept()
+        connect.send(f"Welcome to the chat room!\nThere are {threading.activeCount()} user".encode())
+        s_name = connect.recv(1024)
+        s_name = s_name.decode()
+        if s_name not in list_of_client:
+            list_of_client[connect] = s_name  # contain all client who join the server
+        # threading._start_new_thread(handle_client,(connect,address))
+        user_info = "had connected! :)\n"
+        broadcast(user_info, connect)
+        thread = threading.Thread(target=handle_client, args=(connect, address))
+        #connect.send(f"[ACTIVE CONNECTIONS] {threading.activeCount()}".encode(FORMAT))
+        # thread.daemon = True
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        # connect.send(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}".encode(FORMAT))
+        # thread = threading.Thread(target=handle_client, args=(connect, address))
+        # thread.start()
+        
+      
 
-# name = input(str("Please enter your username : "))
-# print(" Connected to chat server")
-# client.send(name.encode())
+print("[STARTING] server is starting.....")
+start()
 
-print(client.recv(1024).decode())
-name = input(str("Please enter your username : "))
-client.send(name.encode())
-background_receive = threading.Thread(target=receive)
-background_send = threading.Thread(target=send)
-background_receive.daemon = True
-background_send.daemon = True
-background_receive.start()
-background_send.start()
-send()
-
-
-
-
-# while 1:
-#     client.send(input("Please enter your message: ").encode())
-#     print("Sent")
-#     print("")
-
-
-client.close()
