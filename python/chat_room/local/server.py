239af4bfe904f9f34ab29ac403103f39983139a1
Hongce Chen <natechc1@my.yorku.ca>
2021-05-30 18:57:02 -0400

    Update server.py

--- python/chat_room/local/server.py
+++ python/chat_room/local/server.py
@@ -1,24 +1,26 @@
-from socket import AF_INET, socket, SOCK_STREAM
-from threading import Thread
-import time
-from  person  import Person
+import socket
+import threading
+import sys
+import os
 
-# global constant 
-HOST = 'localhost'
+
+HEADER = 16
 PORT = 5050
-ADDR = (HOST, PORT)
 FORMAT = 'utf-8'
 DISCONNECT = "!DISCONNECT"
 # create to store all client and used for sending the msg from other client 
-list_of_person = []
+list_of_client = {}
 
-SERVER = socket(AF_INET, SOCK_STREAM)
-SERVER.bind(ADDR)
-# if SERVER.startswith('127.0.'):
-#     SERVER = '0.0.0.0'
-
+SERVER = socket.gethostbyname(socket.gethostname()) # get the IPv4 address
+if SERVER.startswith('127.0.'):
+    SERVER = '0.0.0.0'
+ADDR = (SERVER, PORT)
 
 #create a new socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+server.bind(ADDR) 
+
+
 
 def handle_client(connect, address):
     print(f"[NEW CONNECTION] {address} connected.")
@@ -34,79 +36,52 @@ def handle_client(connect, address):
     connect.close()      
             
         
-def broadcast(msg, name):
-    for person in list_of_person:
-        client = person.client
-        client.send("[ " + name + " ] " + msg).encode()
-    # try:
-    #     if len(list_of_client) > 1:
-    #         for client in list_of_client:
-    #             if client != conn:
-    #                     new_msg = "[" + str(list_of_client.get(conn)) + "] " + msg + "\n"
-    #                     client.send(new_msg.encode())
-    #     if msg == DISCONNECT:
-    #         list_of_client.pop(conn)    
+def broadcast(msg, conn):
+    try:
+        if len(list_of_client) > 1:
+            for client in list_of_client:
+                if client != conn:
+                        new_msg = "[" + str(list_of_client.get(conn)) + "] " + msg + "\n"
+                        client.send(new_msg.encode())
+        if msg == DISCONNECT:
+            list_of_client.pop(conn)    
 
-    # except BrokenPipeError as e: 
-    #     # Python flushes standard streams on exit; redirect remaining output 
-    #     # to devnull to avoid another BrokenPipeError at shutdown 
-    #     # devnull = os.open(os.devnull, os.O_WRONLY) 
-    #     # os.dup2(devnull, sys.stdout.fileno()) 
-    #     # sys.exit(1)  # Python exits with error code 1 on EPIPE 
-    #     print("[BROADCAST FAILURE] " + e)
+    except BrokenPipeError: 
+        # Python flushes standard streams on exit; redirect remaining output 
+        # to devnull to avoid another BrokenPipeError at shutdown 
+        devnull = os.open(os.devnull, os.O_WRONLY) 
+        os.dup2(devnull, sys.stdout.fileno()) 
+        sys.exit(1)  # Python exits with error code 1 on EPIPE 
                         
 
-def wait_for_connection():
+
+def start():
+    server.listen(5)
+    print(f"[LISTENING] Server is listening on {SERVER}")
     run = True
     while run:
         try:
-            client, addr = SERVER.accept()
-            person = Person(addr, client)
-            list_of_person.append(person )
-            client.send(f"Welcome to the chat room!\nThere are {Thread.activeCount()} user".encode())
-            person = Person()
-            broadcast("had connected! :)\n", client)
-            Thread(target=client_communication, args=(person,)).start()
-            print(f"[ACTIVE CONNECTIONS] {Thread.activeCount() - 1}")
+            connect, address = server.accept()
+            connect.send(f"Welcome to the chat room!\nThere are {threading.activeCount()} user".encode())
+        
+            # get client name part
+            s_name = connect.recv(1024)
+            s_name = s_name.decode()
+            if s_name not in list_of_client:
+                list_of_client[connect] = s_name  # contain all client who join the server
+            # threading._start_new_thread(handle_client,(connect,address))
+            user_had_connected = "had connected! :)\n"
+            broadcast(user_had_connected, connect)
+            thread = threading.Thread(target=handle_client, args=(connect, address))
+            thread.start()
+            print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
         except Exception as e:
             print("[FAILURE] ",e)
             run = False
-    print("SERVER CRASHED!")
-
-
-def client_communication(person):
-    client = person.client
-
-    #set the name part
-    name = client.receive(1024).decode() 
-    broadcast("has joined the chat!" , name)  #broadcast the welcome msg
-
-    while True:
-        try:
-            msg = person.receive(1024).decode()
-            print(f"[ {name} ] " + msg)
-            
-            if msg == "!quit":
-                broadcast(f"{name} has left the chat......", "")  # broadcast the leaving msg
-                # client.send(msg).encode()
-                client.close
-                list_of_person.remove(person)
-                break
-            else:
-                broadcast(msg, name)  # broadcast the normal msg 
-        except Exception as e:
-            print("[FAILURE] ",e)
-            break
-
-
+   
         
-if __name__ == '__main__':
-    SERVER.listen(5)
-    print("[STARTING] server is starting.....")
-    print(f"[LISTENING] Server is listening on {SERVER}")
-    ACCEPT_THREAD = Thread(target=wait_for_connection)
-    ACCEPT_THREAD.start()
-    ACCEPT_THREAD.join()
-    SERVER.close
+      
 
+print("[STARTING] server is starting.....")
+start()
 
